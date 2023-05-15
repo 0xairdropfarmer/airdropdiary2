f8bff0d935b59c142ef726a1114dc6dbad7b85cf
krissanawat101 <krissanawat101@gmail.com>
2023-05-15 07:49:51 +0700

    eveymiunute notification

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('todo:send-due-date-notifications')->daily();
+        $schedule->command('todo:send-due-date-notifications')->everyMinute();
     }
 
     /**
